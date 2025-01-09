8dd651e29bb110c27476e6e76d64b1b1d3be2072
Smilydinesh <146802097+Smilydinesh@users.noreply.github.com>
2025-01-09 12:36:21 +0530

    Update loginUser

--- src/components/loginUser.jsx
+++ src/components/loginUser.jsx
@@ -7,44 +7,6 @@ export default function Login() {
   const [password, setPassword] = useState(''); // State to store password input
   const navigate = useNavigate(); // Hook to navigate after successful login
 
-  // // Handle form submission
-  // const handleSubmit = (e) => {
-  //   e.preventDefault(); // Prevent default form submission
-
-  //   // Prepare login data
-  //   const data = {
-  //     email,
-  //     password,
-  //   };
-
-  //   // Send POST request to login
-  //   axios.post('https://movie-backend-939z.onrender/user/login', data)
-  //     .then((res) => {
-  //       if (res.data.status === 'ok') {
-  //         // On successful login, store token and user info in localStorage
-  //         localStorage.setItem('token', res.data.token);
-  //         localStorage.setItem('user', JSON.stringify(res.data.user)); // Store user data
-
-  //         // Check if the user is an admin
-  //         if (res.data.user.isAdmin) {
-  //           localStorage.setItem('isAdmin', true); // Store admin status
-  //         } else {
-  //           localStorage.setItem('isAdmin', false); // Store admin status
-  //         }
-
-  //         alert('Login Successful');
-  //         // Redirect to the home page
-  //         navigate('/home');
-  //       } else {
-  //         alert(res.data.message || 'Something went wrong');
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       console.error('Error:', err);
-  //       alert('Something went wrong');
-  //     });
-  // };
-
   const handleSubmit = (e) => {
     e.preventDefault(); // Prevent default form submission
   
@@ -86,7 +48,7 @@ export default function Login() {
   
 
   return (
-    <div className="grid place-items-center h-screen bg-[url('../assets/images/backgroundImage.jpg')] bg-cover bg-center">
+    <div className="grid place-items-center h-screen bg-[url('/src/assets/images/backgroundImage.jpg')] bg-cover bg-center">
       {/* Form container with responsive width */}
       <div className="flex justify-center w-full sm:w-4/5 md:w-1/2 lg:w-1/3 h-auto border-2 border-gray-600 rounded-xl bg-gray-400">
         <form onSubmit={handleSubmit}>
